5840b321c5ab80e2966dd677d868ef1ccd6e330d
vagrant <vagrant@homestead>
2019-11-11 03:27:28 +0000

    公司提交

--- app/Http/Controllers/Admin/ConfigController.php
+++ app/Http/Controllers/Admin/ConfigController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Admin\Config;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\ConfigRequest;
+use App\Http\Requests\Base;
 use Illuminate\Http\Request;
 
 class ConfigController extends Controller
@@ -24,7 +26,7 @@ class ConfigController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create(Request $request)
+    public function create(ConfigRequest $request)
     {
         if ($request->ajax()) {
 
